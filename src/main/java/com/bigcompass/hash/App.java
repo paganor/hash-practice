73111d93a1b9403acbcd7d115a4e295c80564303
paganor <rob@bigcompass.com>
2014-11-11 20:03:48 +0000

    maven

---
+++ src/main/java/com/bigcompass/hash/App.java
@@ -0,0 +1,13 @@
+package com.bigcompass.hash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
